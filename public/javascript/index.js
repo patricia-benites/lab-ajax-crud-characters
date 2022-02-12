d1178688b3f63be2bf78f3365b32301c3b09e8cb
Patricia <patricia.benites.cava@gmail.com>
2022-02-12 08:56:16 +0100

    add iteration 3 fetch all and fetcjh one

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,15 +1,35 @@
 const charactersAPI = new APIHandler("http://localhost:8000");
+const fetchAllContainer = document.querySelector(".characters-container");
+
+
+const characterId = document.getElementById("fetch-one-id")
 
 window.addEventListener("load", () => {
   document
     .getElementById("fetch-all")
-    .addEventListener("click", function (event) {
-      charactersAPI.getFullList().then((result) => console.log(result.data))
+    .addEventListener("click", async function (event) {
+      const characters = await charactersAPI.getFullList();
+      const charElements = characters.data
+        .map(
+          (
+            char
+          ) => `<div class="character-info"><div class="name">Character Name: ${char.name}</div>
+                          <div class="occupation">Character Occupation: ${char.occupation}</div>
+                          <div class="cartoon">Is a Cartoon?: ${char.cartoon}</div>
+                          <div class="weapon">Character Weapon: ${char.weapon}</div>
+                        </div>`
+        )
+        .reduce((prev, curr) => prev + curr);
+      fetchAllContainer.innerHTML = charElements;
     });
 
   document
     .getElementById("fetch-one")
-    .addEventListener("click", function (event) {});
+    .addEventListener("click", async function (event) {
+      console.log(characterId.value)
+      const character = await charactersAPI.getOneRegister(characterId.value);
+      console.log(character);
+    });
 
   document
     .getElementById("delete-one")
